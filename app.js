fdac4e32eda0b8fda9e71494addb4ee97494e5ba
Ricardo Tlatelpa <rtlatlepa4@gmail.com>
2020-07-19 17:00:45 -0400

    database connection

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const authRoutes = require('./routes/authRoutes');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
+await mongoose.connect(keys.MONGO_CONNECT_URI);
 
 app.listen(PORT);
-
+app.use(authRoutes);
--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,7 @@
 //production keys
 module.exports = {
     //use process.env to match with deployed environment
-
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,        
+    MONGO_CONNECT_URI: process.env.MONGO_CONNECT_URI
 };
